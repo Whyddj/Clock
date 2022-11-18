b704bb05cf62d19024ddf5fe33db306f456afe53
why <1420652550@qq.com>
2022-11-18 16:06:12 +0800

    UI绘制

---
+++ app/src/main/java/com/why/clock/MainActivity.java
@@ -0,0 +1,37 @@
+package com.why.clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView clock = findViewById(R.id.clock);
+        Button button = findViewById(R.id.button);
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+        String current = time.format(new Date());
+        clock.setText(current);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                @SuppressLint("SimpleDateFormat") SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+                String current = time.format(new Date());
+                clock.setText(current);
+            }
+        });
+    }
+}
