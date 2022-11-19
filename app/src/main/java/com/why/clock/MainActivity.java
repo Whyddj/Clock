6d02933cd3fdb6c2ffe46c85de098897b270b2fe
why <1420652550@qq.com>
2022-11-19 13:44:31 +0800

    界面交互

--- app/src/main/java/com/why/clock/MainActivity.java
+++ app/src/main/java/com/why/clock/MainActivity.java
@@ -4,30 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView clock;
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            handler.postDelayed(this, 1000);
+            clock.setText(getCurrentTime());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView clock = findViewById(R.id.clock);
         Button button = findViewById(R.id.button);
+        clock = findViewById(R.id.clock);
 
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
-        String current = time.format(new Date());
-        clock.setText(current);
+        clock.setText(getCurrentTime());
+
+        handler.post(runnable);
 
         button.setOnClickListener(view -> {
-            String current1 = time.format(new Date());
-            clock.setText(current1);
+            clock.setText(getCurrentTime());
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+
+
+    private String getCurrentTime() {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+        return time.format(new Date());
+    }
+
 }
